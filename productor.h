b18659e3ece28d986c6407d3e63c9c190b7f4106
rohero18 <robertohr18@gmail.com>
2018-04-13 01:22:15 -0400

    Added new changes to the productor.c and creador.c

--- productor.h
+++ productor.h
@@ -1,10 +1,12 @@
 #ifndef PRODUCTOR_H_
 #define PRODUCTOR_H_
+#include "creador.h"
 
 // Definition of struct for the productor
 struct Productor
 {
     int seconds;
+    int productorId;
     char* message;
     char* bufferName;
     int messagesCreated;
@@ -14,5 +16,6 @@ typedef struct Productor Productor;
 
 // Productor.c file methods pre-definitions
 Productor setProductor(int arg, char* argv[]);
+void createMessages(Productor p);
 
 #endif
