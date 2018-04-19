80802d3b5061784b41b5332ea6184b7b410ded7b
rohero18 <robertohr18@gmail.com>
2018-04-19 17:16:41 -0400

    Added test fole to test shared memory segment

--- creador.c
+++ creador.c
@@ -3,41 +3,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "creador.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/mman.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
 
 #define handle_error(msg) \
            do { perror(msg); exit(EXIT_FAILURE); } while (0)
-               
-void* create_shared_memory(size_t size) {
-  // Our memory buffer will be readable and writable:
-  int protection = PROT_READ | PROT_WRITE;
 
-  // The buffer will be shared (meaning other processes can access it), but
-  // anonymous (meaning third-party processes cannot obtain an address for it),
-  // so only this process and its children will be able to use it:
-  int visibility = MAP_ANONYMOUS | MAP_SHARED;
-
-  // The remaining parameters to `mmap()` are not important for this use case,
-  // but the manpage for `mmap` explains their purpose.
-  return mmap(NULL, size, protection, visibility, 0, 0);
-}
-
-// Main method for the productor.c file
 int main(int argc, char* argv[])
 {
-    char* parent_message = "hello";  // parent process will write this message
-    char* child_message = "goodbye"; // child process will then write this one
-
-    void* shmem = create_shared_memory(128);
-
-    memcpy(shmem, parent_message, sizeof(parent_message));
-    while(1);
-
+  int shmid;
+  char* addr1;
+  key_t key;
+  shmid = shmget(1077,sizeof(struct buffer),IPC_CREAT|SHM_R|SHM_W);
+  printf("shmid = %d",shmid);
+  addr1 = shmat(shmid,0,0);
+  printf("\nIPC SHARED MEMORY");
+  //copying your structure at the shared location.
+  
+  struct buffer myObj;
+  myObj.testMessage = "message";
+  
+  memcpy(addr1,&myObj,sizeof(myObj));
+  printf("\nMESSAGE STORED");
+    
 }
 
-// Initializes the variables used in the system
-void initializeVars()
-{
-}
--- creador.h
+++ creador.h
@@ -1,8 +1,12 @@
 #ifndef CREADOR_H_
 #define CREADOR_H_
+#include <sys/shm.h>
+#include <sys/ipc.h>
 
 // Definition of global variables to be used in the system
-
+struct buffer{
+  char* testMessage;
+};
 
 // creador.c file methods pre-definitions
 void initializeVars();
